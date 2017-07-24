9076a75976e802e8936304df2f5225792b096b18
Denis Boyev <susanindin@gmail.com>
2017-07-24 18:00:31 +0600

    some code done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,36 @@ import {TabsPage} from "../pages/tabs/tabs";
 import {SigninPage} from "../pages/signin/signin";
 import {SignupPage} from "../pages/signup/signup";
 import firebase from 'firebase';
+import {AuthService} from "../services/auth";
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  tabsPage: any = TabsPage;
+  rootPage: any = TabsPage;
   signinPage: any = SigninPage;
   signupPage: any = SignupPage;
+  isAuthenticated = false;
   @ViewChild('nav') nav: NavController;
 
   constructor(platform: Platform,
               statusBar: StatusBar,
               splashScreen: SplashScreen,
-              private menuCtrl: MenuController) {
+              private menuCtrl: MenuController,
+              private authService: AuthService) {
     firebase.initializeApp({
       apiKey: "AIzaSyC27gc3A2sksj4gceoMAetkMxt_drUAuhs",
       authDomain: "ionic2-recipebook-be799.firebaseapp.com"
+    });
+    firebase.auth().onAuthStateChanged(user => {
+      if (user){
+        this.isAuthenticated = true;
+        this.rootPage = TabsPage;
+      } else {
+        this.isAuthenticated = false;
+        this.rootPage = SigninPage;
+      }
     });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -39,7 +51,9 @@ export class MyApp {
   }
 
   onLogout() {
-
+    this.authService.logout();
+    this.menuCtrl.close();
+    this.nav.setRoot(SigninPage);
   }
 }
 
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,16 @@ import firebase from 'firebase';
 
 
 export class AuthService {
-  signup(emai: string, password: string) {
-    return firebase.auth().createUserWithEmailAndPassword(emai, password);
+  signup(email: string, password: string) {
+    return firebase.auth().createUserWithEmailAndPassword(email, password);
   }
+
+  signin(email: string, password: string) {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    firebase.auth().signOut();
+  }
+
 }
